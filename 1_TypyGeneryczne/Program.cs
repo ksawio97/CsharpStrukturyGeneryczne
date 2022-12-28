be06d5d2e2048039bac486f606727d2889e63935
ksawio97 <ksawery9708@gmail.com>
2022-12-28 21:53:59 +0100

    1_3 NaszaKolekcja

--- 1_TypyGeneryczne/Program.cs
+++ 1_TypyGeneryczne/Program.cs
@@ -6,10 +6,62 @@ using System.Threading.Tasks;
 
 namespace _1_TypyGeneryczne
 {
-    internal class Program
+    public class KolejkaKolowa
     {
-        static void Main(string[] args)
+        private double[] bufor;
+        private int poczatekBufora;
+        private int koniecBufora;
+
+        public KolejkaKolowa() : this(pojemnosc: 5)
         {
+
+        }
+
+        public KolejkaKolowa(int pojemnosc)
+        {
+            bufor = new double[pojemnosc + 1];
+            poczatekBufora = 0;
+            koniecBufora = 0;
+        }
+
+        public void Zapis(double wartosc)
+        {
+            bufor[koniecBufora] = wartosc;
+            koniecBufora = (koniecBufora + 1) % bufor.Length;
+
+            if (koniecBufora == poczatekBufora)
+                poczatekBufora = (poczatekBufora + 1) % bufor.Length;
+        }
+
+        public double Czytaj()
+        {
+            var wynik = bufor[poczatekBufora];
+            poczatekBufora = (poczatekBufora + 1) % bufor.Length;
+            return wynik;
+        }
+
+        public int Pojemnosc
+        {
+            get
+            {
+                return bufor.Length;
+            }
+        }
+
+        public bool JestPusty
+        {
+            get
+            {
+                return koniecBufora == poczatekBufora;
+            }
+        }
+
+        public bool JestPelny
+        {
+            get
+            {
+                return (koniecBufora + 1) % bufor.Length == poczatekBufora;
+            }
         }
     }
 }
