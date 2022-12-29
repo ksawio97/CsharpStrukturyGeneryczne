fbc3266c03844d73240e7722673e1eeba95d580b
ksawio97 <ksawery9708@gmail.com>
2022-12-29 14:04:34 +0100

    1_5 UnitTest dodałem testy jednostkowe

---
+++ 1_TypyGeneryczneTests/KolejkaKolowaTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using _1_TypyGeneryczne;
+
+namespace _1_TypyGeneryczneTests
+{
+    [TestClass]
+    public class KolejkaKolowaTests
+    {
+        [TestMethod]
+        public void NowaKolejkaJestPusta()
+        {
+            var kolejka = new KolejkaKolowa();
+
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+
+        [TestMethod]
+        public void KolejkaJestPelnaPoTrzechZapisach()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            kolejka.Zapisz(1.1);
+            kolejka.Zapisz(2.3);
+            kolejka.Zapisz(3.4);
+
+            Assert.IsTrue(kolejka.JestPelny);
+        }
+        //FIFO
+        [TestMethod]
+        public void PierwszyWchodziPierwszyWychodzi()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            double[] wartosci = { 1.1, 2.2, 3.3 };
+            
+            foreach(var wartosc in wartosci) 
+                kolejka.Zapisz(wartosc);
+
+
+            foreach (var wartosc in wartosci)
+                Assert.AreEqual(wartosc, kolejka.Czytaj());
+
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+
+        [TestMethod]
+        public void NadipujeGdyJestWiekszaPojemnosc()
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            double[] wartosci = { 1.1, 2.2, 3.3, 4.6, 7.2, 2.1 };
+
+            foreach(var wartosc in wartosci)
+                kolejka.Zapisz(wartosc);
+
+            Assert.IsTrue(kolejka.JestPelny);
+
+            for(int i = 3; i < wartosci.Length;i++)
+                Assert.AreEqual(wartosci[i], kolejka.Czytaj());
+
+            Assert.IsTrue(kolejka.JestPusty);
+        }
+    }
+}
---
+++ 1_TypyGeneryczneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("1_TypyGeneryczneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1_TypyGeneryczneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7fe579ca-b669-4830-ac9c-2f32c1525bef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
