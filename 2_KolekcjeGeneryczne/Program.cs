fec44b81db34e437bd82c1cce5d307f4a34658c2
ksawio97 <ksawery9708@gmail.com>
2023-01-10 12:37:23 +0100

    przechowywanie pracowników w array

--- 2_KolekcjeGeneryczne/Program.cs
+++ 2_KolekcjeGeneryczne/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http.Headers;
 
 namespace _2_KolekcjeGeneryczne
 {
@@ -10,6 +8,22 @@ namespace _2_KolekcjeGeneryczne
     {
         static void Main(string[] args)
         {
+            Pracownik[] pracownicy = new Pracownik[]
+            {
+                new Pracownik { Imie = "Marcin", Nazwisko = "Nowak"},
+                new Pracownik { Imie = "Jan", Nazwisko = "Nowak" },
+                new Pracownik { Imie = "Tomasz", Nazwisko = "Nowak" }
+            };
+
+            foreach (var pracownik in pracownicy)
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
+            Console.WriteLine("---------");
+
+            Array.Resize(ref pracownicy, pracownicy.Length + 1);
+            pracownicy[pracownicy.Length] = new Pracownik { Imie = "Anatol", Nazwisko = "Koszyk" };
+
+            for (int i = 0; i < pracownicy.Length; i++)
+                Console.WriteLine(pracownicy[i].Imie + " " + pracownicy[i].Nazwisko);
         }
     }
 }
