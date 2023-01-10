4037797d959949c7a30d71e9a6d6e0a100d6f14e
ksawio97 <ksawery9708@gmail.com>
2023-01-10 13:00:07 +0100

    dodanie testów listy

---
+++ 2_KolekcjeGeneryczneTests/ListaTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2_KolekcjeGeneryczneTests
+{
+    [TestClass]
+    public class ListaTests
+    {
+        [TestMethod]
+        public void ListaMozemyDodawacNaKoniec()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+            listaLiczb.Add(4);
+
+            Assert.AreEqual(4, listaLiczb.Count);
+        }
+
+        [TestMethod]
+        public void ListaMozemyDodawacNaPozycji()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+            listaLiczb.Insert(1, 8);
+
+            Assert.AreEqual(8, listaLiczb[1]);
+        }
+
+        [TestMethod]
+        public void ListaMozemyUsuwacNaPozycji()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+            listaLiczb.RemoveAt(1);
+
+            Assert.AreEqual(3, listaLiczb[1]);
+        }
+
+        [TestMethod]
+        public void ListaMozemyUsuwacElement()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+            listaLiczb.Remove(1);
+
+            Assert.IsTrue(listaLiczb.SequenceEqual(new[] { 2, 3 } ));
+        }
+
+        [TestMethod]
+        public void ListaMozemyWyszukiwac()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+
+            Assert.AreEqual(2, listaLiczb.IndexOf(3));
+        }
+
+        [TestMethod]
+        public void ListaCzyJestElement()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+
+            Assert.IsTrue(listaLiczb.Contains(2));
+        }
+
+        [TestMethod]
+        public void ListaDodajemyNaKoniecZakresLiczb()
+        {
+            List<int> listaLiczb = new List<int> { 1, 2, 3 };
+
+            listaLiczb.AddRange(new[] { 4, 5, 6 });
+            Assert.AreEqual(6, listaLiczb[5]);
+        }
+    }
+}
---
+++ 2_KolekcjeGeneryczneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("2_KolekcjeGeneryczneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2_KolekcjeGeneryczneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89314dba-1f3a-4225-94d0-4b807264423d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
