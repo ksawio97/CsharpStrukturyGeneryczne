ac9f692c5eab247fb80c59537a267670c2130ac7
ksawio97 <ksawery9708@gmail.com>
2023-01-10 13:53:15 +0100

    dodanie testów stosu i zaimplementowanie stosu dla pracowników

--- 2_KolekcjeGeneryczne/Program.cs
+++ 2_KolekcjeGeneryczne/Program.cs
@@ -7,6 +7,8 @@ namespace _2_KolekcjeGeneryczne
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kolejka: ");
+
             Queue<Pracownik> kolejka = new Queue<Pracownik>();
             kolejka.Enqueue(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
             kolejka.Enqueue(new Pracownik { Imie = "Marek", Nazwisko = "Nowak" });
@@ -16,6 +18,20 @@ namespace _2_KolekcjeGeneryczne
             while(0 < kolejka.Count)
             {
                 var pracownik = kolejka.Dequeue();
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
+            }
+
+            Console.WriteLine("\nStos:");
+
+            Stack<Pracownik> stos = new Stack<Pracownik>();
+            stos.Push(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
+            stos.Push(new Pracownik { Imie = "Marek", Nazwisko = "Nowak" });
+            stos.Push(new Pracownik { Imie = "Zosia", Nazwisko = "Nowak" });
+            stos.Push(new Pracownik { Imie = "Ola", Nazwisko = "Nowak" });
+
+            while(0 < stos.Count)
+            {
+                var pracownik = stos.Pop();
                 Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
             }
         }
---
+++ 2_KolekcjeGeneryczneTests/StosTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_KolekcjeGeneryczneTests
+{
+    [TestClass]
+    public class StosTests
+    {
+        private Stack<int> StackGenerate(int quantity = 5)
+        {
+            Stack<int> kol = new Stack<int>();
+            for (int i = 1; i <= quantity; i++)
+                kol.Push(i);
+
+            return kol;
+        }
+
+
+        [TestMethod]
+        public void UzyciePeek()
+        {
+            var kolejka = StackGenerate();
+
+            Assert.AreEqual(5, kolejka.Peek());
+        }
+
+
+        [TestMethod]
+        public void UzycieContains()
+        {
+            var kolejka = StackGenerate();
+
+            Assert.IsTrue(kolejka.Contains(2));
+        }
+
+        [TestMethod]
+        public void UzycieToArray()
+        {
+            var kolejka = StackGenerate();
+
+            int[] tablica = kolejka.ToArray();
+            kolejka.Pop();
+
+            Assert.AreEqual(5, tablica[0]);
+            Assert.AreEqual(4, kolejka.Count);
+        }
+
+        [TestMethod]
+        public void UzycieClear()
+        {
+            var kolejka = StackGenerate();
+
+            kolejka.Clear();
+
+            Assert.AreEqual(kolejka.Count, 0);
+        }
+    }
+}
